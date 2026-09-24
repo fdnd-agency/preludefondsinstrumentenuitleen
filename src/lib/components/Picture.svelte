b12c8d1774dd6e647b50b402a2c0f5827b1374d1
Tjeerd <tj.wokke@gmail.com>
2026-09-24 16:57:16 +0200

    picture partial added #15

---
+++ src/lib/components/Picture.svelte
@@ -0,0 +1,31 @@
+<script>
+    let { src, width, height, alt } = $props();
+    src = "https://fdnd-agency.directus.app/assets/" + src;
+</script>
+
+<p>{src}</p>
+
+<picture>
+    <source
+        srcset={`${src}?width=${width}&height=${height}&format=avif&quality=70`}
+        type="image/avif"
+    />
+    <source
+        srcset={`${src}?width=${width}&height=${height}&format=webp&quality=70`}
+        type="image/webp"
+    />
+    <img src={`${src}?width=${width}&height=${height}&quality=60`} {alt} />
+</picture>
+
+<!-- 
+This is how you use the picture partial
+    import Picture from "$lib/components/Picture.svelte";
+
+    <Picture
+        src={id of picture in the database}
+        width="250"
+        height="250"
+        alt={Alt text}
+    />
+
+-->
